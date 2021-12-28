81d9bde7d16111c4013e072855deabcb7f33bffc
bikay K <qhrud5477@gmail.com>
2021-12-28 14:51:09 +0900

    bk

--- Team5_XN_DAC/ProcessDAC.cs
+++ Team5_XN_DAC/ProcessDAC.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Team5_XN_DAC
 {
-    public class ProcessDAC
+    public class ProcessDAC : IDisposable
     {
+        SqlConnection conn;
+        public ProcessDAC()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["local"].ConnectionString);
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Team5_XN_DAC/WorkCenterDAC.cs
+++ Team5_XN_DAC/WorkCenterDAC.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace Team5_XN_DAC
 {
-    public class WorkCenterDAC
+    public class WorkCenterDAC : IDisposable
     {
+        SqlConnection conn;
+        public WorkCenterDAC()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["local"].ConnectionString);
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
